59a549bf20485b43975311a973c4ebc98edf1cc7
Armin Klotz <armin.klotz@hotmail.com>
2021-05-31 14:12:09 +0100

    Initial push

---
+++ first_package/lib.py
@@ -0,0 +1,7 @@
+from time import sleep
+
+def try_me():
+    name = input("Hi! What's your name?\n")
+    print(f"Hi {name}!\n")
+    sleep(1)
+    print(f"This program does nothing.\nHave a nice day!")
